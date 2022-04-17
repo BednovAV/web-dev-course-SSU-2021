dec68f458ef8c80a02d0e0f170888ec58be0a4fb
Andrei Bednov <Andrei_Bednov@epam.com>
2022-04-17 17:33:42 +0400

    Development of game logic - implement gameplay

---
+++ Task-10_GameDev/sc.js
@@ -0,0 +1,277 @@
+// -----------------------------Constants------------------------------------
+
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+const pointsOutput = document.getElementById('pointsOutput');
+
+const figures = {
+  'I': [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ],
+  'J': [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  'L': [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  'O': [
+    [1, 1],
+    [1, 1],
+  ],
+  'S': [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+  'Z': [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  'T': [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ]
+};
+
+const figureColors = {
+  'I': 'cyan',
+  'O': 'yellow',
+  'T': 'purple',
+  'S': 'green',
+  'Z': 'red',
+  'J': 'blue',
+  'L': 'orange'
+};
+
+const grid = 32;
+
+// -----------------------------End Constants------------------------------------
+
+// -----------------------------Helpers------------------------------------------
+
+function GetEmptyPlayField() {
+  var playfield = [];
+
+  for (let row = -2; row < 20; row++) {
+    playfield[row] = [];
+
+    for (let col = 0; col < 10; col++) {
+      playfield[row][col] = 0;
+    }
+  }
+
+  return playfield;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function rotate(matrix) {
+  const N = matrix.length - 1;
+  const result = matrix.map((row, i) =>
+    row.map((val, j) => matrix[N - j][i])
+  );
+  return result;
+}
+
+function isValidMove(matrix, cellRow, cellCol) {
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] && (
+        cellCol + col < 0 ||
+        cellCol + col >= playfield[0].length ||
+        cellRow + row >= playfield.length ||
+        playfield[cellRow + row][cellCol + col])
+      ) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function generateSequence(tetrominoSequence) {
+  const sequence = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+
+  while (sequence.length) {
+    const rand = getRandomInt(0, sequence.length - 1);
+    const name = sequence.splice(rand, 1)[0];
+    tetrominoSequence.push(name);
+  }
+}
+
+// ----------------------------- End Helpers------------------------------------------
+
+function keydownListener(e) {
+  if (gameOver) return;
+
+  if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+    const col = e.key === "ArrowLeft"
+      ? tetromino.col - 1
+      : tetromino.col + 1;
+
+    if (isValidMove(tetromino.matrix, tetromino.row, col)) {
+      tetromino.col = col;
+    }
+  }
+
+  if (e.key === "ArrowUp") {
+    const matrix = rotate(tetromino.matrix);
+    if (isValidMove(matrix, tetromino.row, tetromino.col)) {
+      tetromino.matrix = matrix;
+    }
+  }
+
+  if (e.key === "ArrowDown") {
+    const row = tetromino.row + 1;
+    if (!isValidMove(tetromino.matrix, row, tetromino.col)) {
+      tetromino.row = row - 1;
+      placeTetromino();
+      return;
+    }
+    tetromino.row = row;
+  }
+}
+
+var tetrominoSequence = [];
+
+var playfield = GetEmptyPlayField();
+
+let count = 0;
+let points = 0;
+let tetromino = getNextTetromino();
+let rAF = null;
+let gameOver = false;
+
+function getNextTetromino() {
+  if (tetrominoSequence.length === 0) {
+    generateSequence(tetrominoSequence);
+  }
+  const name = tetrominoSequence.pop();
+  const matrix = figures[name];
+
+  const col = playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
+
+  const row = name === 'I' ? -1 : -2;
+
+  return {
+    name: name,
+    matrix: matrix,
+    row: row,
+    col: col
+  };
+}
+
+function placeTetromino() {
+  for (let row = 0; row < tetromino.matrix.length; row++) {
+    for (let col = 0; col < tetromino.matrix[row].length; col++) {
+      if (tetromino.matrix[row][col]) {
+
+        if (tetromino.row + row < 0) {
+          return showGameOver();
+        }
+        playfield[tetromino.row + row][tetromino.col + col] = tetromino.name;
+      }
+    }
+  }
+
+  let clearedRows = 0;
+  for (let row = playfield.length - 1; row >= 0;) {
+    if (playfield[row].every(cell => !!cell)) {
+      for (let r = row; r >= 0; r--) {
+        for (let c = 0; c < playfield[r].length; c++) {
+          playfield[r][c] = playfield[r - 1][c];
+        }
+      }
+      clearedRows ++;
+    }
+    else {
+      row--;
+    }
+  }
+  IncrementPoints(clearedRows);
+  tetromino = getNextTetromino();
+}
+
+function IncrementPoints(clearedRows){
+  switch (clearedRows) {
+    case 1:
+      points += 100;
+      break;
+    case 2:
+      points += 300;
+      break;
+    case 3:
+      points += 700;
+      break;
+    case 4:
+      points += 1500;
+      break;
+
+  } 
+}
+
+function showGameOver() {
+  cancelAnimationFrame(rAF);
+  gameOver = true;
+  pointsOutput.innerText = `GAME OVER! You scored ${points} points.`;
+}
+
+
+
+function gameLoop() {
+  rAF = requestAnimationFrame(gameLoop);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let row = 0; row < 20; row++) {
+    for (let col = 0; col < 10; col++) {
+      if (playfield[row][col]) {
+        const name = playfield[row][col];
+        context.fillStyle = figureColors[name];
+
+        context.fillRect(col * grid, row * grid, grid - 1, grid - 1);
+      }
+    }
+  }
+
+  if (tetromino) {
+
+    if (++count > 35) {
+      tetromino.row++;
+      count = 0;
+
+      if (!isValidMove(tetromino.matrix, tetromino.row, tetromino.col)) {
+        tetromino.row--;
+        placeTetromino();
+      }
+    }
+
+    context.fillStyle = figureColors[tetromino.name];
+
+    for (let row = 0; row < tetromino.matrix.length; row++) {
+      for (let col = 0; col < tetromino.matrix[row].length; col++) {
+        if (tetromino.matrix[row][col]) {
+          context.fillRect((tetromino.col + col) * grid, (tetromino.row + row) * grid, grid - 1, grid - 1);
+        }
+      }
+    }
+  }
+
+  pointsOutput.innerHTML = `Points: ${points}`;
+}
+
+document.addEventListener('keydown', keydownListener);
+rAF = requestAnimationFrame(gameLoop);
